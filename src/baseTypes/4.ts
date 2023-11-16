220b1b46b1862642546f5921fdc505c7f94f7c1f
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-11-16 15:47:49 +0200

    +++

--- src/baseTypes/4.ts
+++ src/baseTypes/4.ts
@@ -2,7 +2,9 @@
   Як переписати його в TypeScript, використовуючи концепцію кортежів, 
   щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
 */
+let person: [string, number];
+person = ["Max", 21];
 
-let person = ['Max', 21];
+// let person: [string, number] = ['Max', 21];
 
 export {};
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,22 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Weekdays {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function isWeekend(day: Weekdays): boolean {
+  return day === Weekdays.Saturday || day === Weekdays.Sunday;
+}
+
+const today = Weekdays.Friday;
+const result = isWeekend(today);
+
+console.log(result);
